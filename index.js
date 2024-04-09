a9708520ee1a037ab43d1b499f80a61c277528fb
Kelvin <frednjogu22@gmail.com>
2024-04-09 11:12:11 +0300

    array filter

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+//Returns a list of drivers whose names match the provided string (case insensitive)
+function findMatching(drivers, name){
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+// Returns a list of drivers whose names begin with the provided letters
+function fuzzyMatch(drivers, query){
+    return drivers.filter(driver => driver.startsWith(query));
+}
+//Returns a list of drivers whose name property matches the provided string
+function matchName(drivers, name){
+    return drivers.filter(driver => driver.name === name);
+}
+  
+ 
+ 
